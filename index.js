55b4bd9c81402f3ed152dbae5c5d84133b421006
ygguorun <ygguorun@163.com>
2022-06-25 11:04:05 +0800

    Add axmath, axglyph

--- index.js
+++ index.js
@@ -6,7 +6,22 @@ addEventListener('fetch', event => {
  * @param {Request} request
  */
 async function handleRequest(request) {
-  const latestVersion = await effiditCheckver()
+  console.log('fetch event:', request.url.split('/'))
+  softwareName = request.url.split('/')[3]
+  var latestVersion = '404'
+  switch (softwareName) {
+    case 'effidit':
+      latestVersion = await effiditCheckver()
+      break;
+    case 'axglyph':
+      latestVersion = await axglyphCheckver()
+      break;
+    case 'axmath':
+      latestVersion = await axmathCheckver()
+      break;
+    default:
+      latestVersion = '404'
+  }
   return new Response(latestVersion, {
     headers: { 'content-type': 'text/plain' },
   })
@@ -33,5 +48,56 @@ async function effiditCheckver() {
   })).text()
   latestVersion = versionReg.exec(jsBody)
   // console.log(latestVersion)
-  return latestVersion[1]
+  return JSON.stringify({
+    name: 'effidit',
+    version: latestVersion[1]
+  })
+}
+
+async function axglyphCheckver() {
+  const baseUrl = 'https://www.amyxun.com'
+  var versionReg = /AxGlyphV([\d.]+)/g;
+  var sha256Reg = /SHA256: ([A-Fa-f0-9]{64})/g;
+  var downloadUrlReg = /href='(.*?AxGlyph_Setup_Win.*?zip.*?)'/g;
+  var homeBody = await (await fetch(baseUrl, {
+    method: 'GET',
+    headers: {
+      'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.0.0 Safari/537.36'
+    }
+  })).text();
+
+  var version = versionReg.exec(homeBody)[1];
+  // 绕过第一个
+  console.log(sha256Reg.exec(homeBody))
+  var sha256 = sha256Reg.exec(homeBody)[1];
+  var downloadUrl = downloadUrlReg.exec(homeBody)[1];
+  return JSON.stringify({
+    name: 'axglyph',
+    version: version,
+    sha256: sha256,
+    downloadUrl: downloadUrl
+  })
+}
+
+async function axmathCheckver() {
+  const baseUrl = 'https://www.amyxun.com'
+  var versionReg = /AxMathV([\d.]+)/g;
+  var sha256Reg = /SHA256: ([A-Fa-f0-9]{64})/g;
+  var downloadUrlReg = /href='(.*?AxMath_Setup_Win.*?zip.*?)'/g;
+  var homeBody = await (await fetch(baseUrl, {
+    method: 'GET',
+    headers: {
+      'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.0.0 Safari/537.36'
+    }
+  })).text();
+
+  var version = versionReg.exec(homeBody)[1];
+  var sha256 = sha256Reg.exec(homeBody)[1];
+  var downloadUrl = downloadUrlReg.exec(homeBody)[1];
+  return JSON.stringify({
+    name: 'axmath',
+    version: version,
+    sha256: sha256,
+    downloadUrl: downloadUrl
+  })
 }
